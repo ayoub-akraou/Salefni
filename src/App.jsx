d4b2aab1d5b2adc4db95a0763c4df098fc619ffd
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-07 09:55:35 +0100

    feat(App.jsx): add /loan-simulation route that leads to LoanSimulation page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import LoanSimulation  from "./pages/guest/LoanSimulation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="/loan-simulation" element={<LoanSimulation />} />
 			</Routes>
 		</BrowserRouter>
 	);
