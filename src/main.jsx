b7f26afcbe5a50a1bc036401961e31c9d1b3669e
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-09 11:03:43 +0100

    feat: provide AppData state to the App

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./styles/output.css";
 import App from "./App.jsx";
 
+import AppDataContextProvider from "./context/AppDataContext";
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<AppDataContextProvider>
+			<App />
+		</AppDataContextProvider>
 	</StrictMode>
 );
