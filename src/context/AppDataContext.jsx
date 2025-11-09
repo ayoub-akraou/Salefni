5bba2b407f474093572be738e2642593d1a42309
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-09 23:21:06 +0100

    feat: optimize the data consumpttion

--- src/context/AppDataContext.jsx
+++ src/context/AppDataContext.jsx
@@ -4,72 +4,83 @@ import axios from "axios";
 export const AppDataContext = createContext();
 
 export default function AppDataContextProvider({ children }) {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-    const [creditTypes, setCreditTypes] = useState([]);
-    const [employmentTypes, setEmploymentTypes] = useState([]);
-    const [jobs, setJobs] = useState([]);
-    const [applications, setApplications] = useState([]);
-    const [simulations, setSimulations] = useState([]);
-    const [notifications, setNotifications] = useState([]);
-    const [admins, setAdmins] = useState([]);
-    const [settings, setSettings] = useState([]);
+	const [creditTypes, setCreditTypes] = useState([]);
+	const [employmentTypes, setEmploymentTypes] = useState([]);
+	const [jobs, setJobs] = useState([]);
+	const [applications, setApplications] = useState([]);
+	const [simulations, setSimulations] = useState([]);
+	const [notifications, setNotifications] = useState([]);
+	const [admins, setAdmins] = useState([]);
+	const [settings, setSettings] = useState([]);
 
-    useEffect(() => {
-        const api = axios.create({
-          baseURL: "http://localhost:3001",
-        });
-        (async function loadData() {
-            setLoading(true);
-            try {
-                const res = await api.get("/data");
-                setCreditTypes(res.data.creditTypes);
-                setEmploymentTypes(res.data.employmentTypes);
-                setJobs(res.data.jobs);
-                setApplications(res.data.applications);
-                setSimulations(res.data.simulations);
-                setNotifications(res.data.notifications);
-                setAdmins(res.data.admins);
-                setSettings(res.data.settings);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
-        })()
-    }, []);
+	useEffect(() => {
+		const api = axios.create({
+			baseURL: "http://localhost:3001",
+		});
+		(async function loadData() {
+			setLoading(true);
+			try {
+				const [creditTypes, employmentTypes, jobs, applications, simulations, notifications, admins, settings] =
+					await Promise.all([
+						api.get("/creditTypes"),
+						api.get("/employmentTypes"),
+						api.get("/jobs"),
+						api.get("/applications"),
+						api.get("/simulations"),
+						api.get("/notifications"),
+						api.get("/admins"),
+						api.get("/settings"),
+					]);
 
-    return (
-      <AppDataContext.Provider
-        value={{
-          creditTypes,
-          setCreditTypes,
-          employmentTypes,
-          setEmploymentTypes,
-          jobs,
-          setJobs,
-          applications,
-          setApplications,
-          simulations,
-          setSimulations,
-          notifications,
-          setNotifications,
-          admins,
-          setAdmins,
-          settings,
-          setSettings,
-          loading,
-          setLoading,
-          error,
-          setError,
-        }}
-      >
-        {children}
-      </AppDataContext.Provider>
-    );
+				setCreditTypes(creditTypes.data);
+				setEmploymentTypes(employmentTypes.data);
+				setJobs(jobs.data);
+				setApplications(applications.data);
+				setSimulations(simulations.data);
+				setNotifications(notifications.data);
+				setAdmins(admins.data);
+				setSettings(settings.data);
+				console.log(creditTypes.data);
+			} catch (error) {
+				setError(error);
+			} finally {
+				setLoading(false);
+			}
+		})();
+	}, []);
+
+	return (
+		<AppDataContext.Provider
+			value={{
+				creditTypes,
+				setCreditTypes,
+				employmentTypes,
+				setEmploymentTypes,
+				jobs,
+				setJobs,
+				applications,
+				setApplications,
+				simulations,
+				setSimulations,
+				notifications,
+				setNotifications,
+				admins,
+				setAdmins,
+				settings,
+				setSettings,
+				loading,
+				setLoading,
+				error,
+				setError,
+			}}>
+			{children}
+		</AppDataContext.Provider>
+	);
 }
 
 export function useAppData() {
-    return useContext(AppDataContext);
+	return useContext(AppDataContext);
 }
